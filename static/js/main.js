e17c017ca9ef1bc706c4075962422c00251dbd59
Jonathan Routley <jonathan.wei.liang@gmail.com>
2019-09-23 18:10:39 +0100

    major updates

--- static/js/main.js
+++ static/js/main.js
@@ -8,18 +8,17 @@ var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+var mainPage = document.getElementById("main_page")
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNavbar() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
+        mainPage.classList.add("buffer");
     } 
     else {
         navbar.classList.remove("sticky");
+        mainPage.classList.remove("buffer");
     }
 }
 
-
-// Return Button
-function goBack() {
-    window.history.back();
-  }
